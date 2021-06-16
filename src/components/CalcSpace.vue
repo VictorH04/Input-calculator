72ef1d4e584ebb150be4daee4f054af4d756400e
Victor Hristov <Victor.h7@live.com>
2021-06-16 22:16:03 +0200

    Added a select for the different methods but going to use buttons

--- src/components/CalcSpace.vue
+++ src/components/CalcSpace.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="calcSpace">
-    <form @submit.prevent="getAnswer">
+    <form v-on:keyup.enter.prevent="getAnswer">
       <Calc1 :calc1Value="Value1" v-model="Value1" @getInput="getInput()" />
+      <SelectVal
+        :calcMethod="this.chosenCalc"
+        :calcMethodList="this.calcList"
+      />
       <Calc2 :calc2Value="Value2" v-model="Value2" @getInput="getInput()" />
       <sumContainer :calcSum="calcSum" />
 
       <button class="calcSpace-btn" @click.prevent="getAnswer">
         Calculate
       </button>
+
+      <span>Chosen: {{ this.chosenCalc }}</span>
     </form>
   </div>
 </template>
@@ -16,6 +22,7 @@
 import Calc1 from "../components/inputs/Calc1.vue";
 import Calc2 from "../components/inputs/Calc2.vue";
 import sumContainer from "../components/Sum/SumContainer.vue";
+import SelectVal from "../components/SelectCalc/SelectVal.vue";
 
 export default {
   name: "calcSpace",
@@ -24,6 +31,7 @@ export default {
     Calc1,
     Calc2,
     sumContainer,
+    SelectVal,
   },
 
   data() {
@@ -31,6 +39,9 @@ export default {
       Value1: 0,
       Value2: 0,
       calcSum: 0,
+      chosenCalc: "",
+
+      calcList: ["+", "-", "*", "/"],
     };
   },
 
---
+++ src/components/SelectCalc/SelectVal.vue
@@ -0,0 +1,23 @@
+<template>
+  <select class="selectVal" v-bind:value="calcMethod" required>
+    <option v-for="item in calcMethodList" :key="item">{{ item }}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "SelectVal",
+
+  props: {
+    calcMethod: {
+      required: true,
+    },
+
+    calcMethodList: {
+      reqired: true,
+    },
+  },
+};
+</script>
+
+<style></style>
